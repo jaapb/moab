20764b67d2437450860d74bd085b182c351132f5
Jaap Boender <jaapb@kerguelen.org>
2018-10-25 11:29:26 +0100

    Fixed bug in schedule generation

--- moab_presentation_db.ml
+++ moab_presentation_db.ml
@@ -8,13 +8,13 @@ let get_schedule ayear group_number =
 			LEFT JOIN moab.presentation_schedule ps1 ON ps1.learning_week = gs.week \
 				AND ps1.first_presenter \
 				AND ps1.academic_year = $ayear \
+				AND ps1.group_number = $group_number \
 			LEFT JOIN moab.students s1 ON ps1.userid = s1.userid \
 			LEFT JOIN moab.presentation_schedule ps2 ON ps2.learning_week = gs.week \
 				AND NOT ps2.first_presenter \
 				AND ps2.academic_year = $ayear \
+				AND ps2.group_number = $group_number \
 			LEFT JOIN moab.students s2 ON ps2.userid = s2.userid \
-		WHERE (s1.group_number = $group_number OR s1.group_number IS NULL) \
-		AND (s2.group_number = $group_number OR s2.group_number IS NULL) \
 		ORDER BY gs.week ASC") >>=
 	Lwt_list.map_s (function
 	| (Some w, uid1, uid2) -> Lwt.return (w, uid1, uid2)
